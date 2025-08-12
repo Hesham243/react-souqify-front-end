7b2ed9d3ee8926a44bd874340270539f3b1ef1d0
sara-jaffar <sara.alshoula55@email.com>
2025-08-12 18:26:08 +0300

    added StoreDetails component with its routes and added Link to the StoreList component

--- src/components/StoreList/StoreList.jsx
+++ src/components/StoreList/StoreList.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const StoreList = (props) => {
   return(
     <main>
       <h1>Store List</h1>
       {props.stores.map((store) => (
         <div key={store._id} className="store-card">
+        <Link to={`/stores/${store._id}`} >
           {store.logo ? (
             <img 
               src={store.logo} 
@@ -13,6 +16,7 @@ const StoreList = (props) => {
           ) : null}
           <h3>{store.name}</h3>
           <p><strong>Category:</strong> {store.category}</p>
+          </Link>
         </div>
       ))}
     </main>
