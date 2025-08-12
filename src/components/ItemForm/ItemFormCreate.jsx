a5ccce7944ddfca6155a0eb01e0d07ffbef93683
Husain <halsehlawi@outlook.com>
2025-08-13 02:24:37 +0300

    Added new routes to navBar

--- src/components/ItemForm/ItemFormCreate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ItemFormCreate = () => {
-  return <div>Create Item Form</div>;
-};
-
-export default ItemFormCreate;
--- src/components/ItemForm/ItemFormUpdate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ItemFormUpdate = () => {
-  return <div>Update Item Form</div>;
-};
-
-export default ItemFormUpdate;
